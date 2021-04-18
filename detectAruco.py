7b8d48f97bf0960e0a3304fa44813e61d6d6b249
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2021-04-18 17:37:38 +0200

    Added file to return aruco coordinates from image

---
+++ detectAruco.py
@@ -0,0 +1,30 @@
+import cv2
+import statistics as stat
+
+
+def get_aruco(img, dictionary=cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)):
+    '''Detects aruco markers in cv2 input image and returns dictionary with id and 
+    center points. If no markers are found returns None'''
+
+    # Detects markers based on image and dictionary with standars parameters
+    markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(
+        cv2.cvtColor(img, cv2.COLOR_BGR2RGB),
+        dictionary,
+        parameters=cv2.aruco.DetectorParameters_create()
+    )
+
+    ps = {}
+    for i, sq in enumerate(markerCorners):
+        sq = sq[0]                                  # Unpacks square
+        ID = str(*markerIds[i])                     # Gets markers id
+
+        cen_x = int(stat.mean([x[0] for x in sq]))  # Centerpoint is mean value
+        cen_y = int(stat.mean([y[1] for y in sq]))
+
+        ps[int(ID)] = (cen_x, cen_y)                # Save to dict id: (x, y)
+
+    return ps if ps else None                       # If points are empty, return None
+
+if '__main__'==__name__:
+    img = cv2.imread('markers_per.jpg')
+    print(get_aruco(img))
