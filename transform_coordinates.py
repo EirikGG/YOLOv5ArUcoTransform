0f7e544ff598f3f0956f8b2762b61008a51296c8
EirikGG <eirikgg94@gmail.com>
2021-05-25 11:08:28 +0200

    Restrucktured and removed unnecesary files

---
+++ transform_coordinates.py
@@ -0,0 +1,76 @@
+import cv2
+import statistics as stat
+
+
+def get_aruco_pos(img, dictionary=cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)):
+    '''Detects aruco markers in cv2 input image and returns dictionary with id and 
+    center points. If no markers are found returns None'''
+
+    # Detects markers based on image and dictionary with standars parameters
+    markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(
+        cv2.cvtColor(img, cv2.COLOR_BGR2RGB),
+        dictionary,
+        parameters=cv2.aruco.DetectorParameters_create()
+    )
+
+    ps = {}
+    for i, sq in enumerate(markerCorners):
+        sq = sq[0]                                  # Unpacks square
+        ID = str(*markerIds[i])                     # Gets markers id
+
+        cen_x = int(stat.mean([x[0] for x in sq]))  # Centerpoint is mean value
+        cen_y = int(stat.mean([y[1] for y in sq]))
+
+        ps[int(ID)] = (cen_x, cen_y)                # Save to dict id: (x, y)
+
+
+    return ps if 4==len(ps) else None               # If square is not found, return None
+
+
+def transform_to_new_coord(point, pixel_coords, res=(100,100)):
+    '''Transforms a point to a new coodinate system based on
+    pixel coordinates, which are assumed to be the corners of a
+    square'''
+    src = np.float32(pixel_coords)                  # Source square
+    
+    dst = np.float32([                              # Destination square
+        [0,0],
+        [res[0]-1,0],
+        [res[0]-1,res[1]-1],
+        [0,res[1]-1]
+    ])
+
+    h, _ = cv2.findHomography(src, dst)             # Find transformation matrix
+    
+    new_p = np.dot((point[0], point[1], 1), h.T)    # Get new point
+
+    new_p = new_p/new_p[-1]                         # Remove omega value
+
+    return *new_p[:-1], h
+
+def get_coords(img, point2d, new_coord_res=(100,100)):
+    aruco_points = get_aruco_pos(img)
+
+    x, y, h = None, None, None
+    if aruco_points:
+        x, y, h = transform_to_new_coord(
+            point=point2d,
+            pixel_coords=(
+                aruco_points[0],
+                aruco_points[1],
+                aruco_points[2],
+                aruco_points[3]),
+            res=new_coord_res
+        )
+
+    if any((x, y)):
+        x,y = tuple(map(int, (x, y)))
+    
+    return (x, y), aruco_points, h
+
+
+
+if '__main__'==__name__:
+    img = cv2.imread('markers.png')
+
+    print(get_coords(img, (430, 451), (1000, 1000)))
---
+++ yolo_inference.py
@@ -0,0 +1,86 @@
+import torch, cv2
+
+import numpy as np
+
+from transform_coordinates import get_coords
+
+def add_border(img, size=(10, 10, 10, 10)):
+    '''Adds borders to an image '''
+    return cv2.copyMakeBorder(
+        img,
+        *size,
+        borderType=cv2.BORDER_CONSTANT
+    )
+
+def get_empty_img(map_size = (1000, 1000)):
+    '''Returns an empty white image'''
+    map_img = np.zeros((map_size[0], map_size[1],3), np.uint8)
+    map_img.fill(255)
+    return map_img
+
+
+yolov5s = torch.hub.load(                               # Load yolo model
+    'ultralytics/yolov5', 
+    'custom', 
+    path_or_model='yolos_weights/best.pt'
+)
+
+#yolov5s = yolov5s.fuse().autoshape()
+
+cap = cv2.VideoCapture(0)
+
+map_size = (1000, 1000)                                 # Initialize map
+map_img = get_empty_img(map_size)
+
+while True:
+    _, frame = cap.read()                               # Input frame
+    #frame = cv2.imread('testImg/markers.png')
+
+    out = yolov5s(frame)                                # Get predictions
+
+    out_df = out.pandas().xyxy[0]                       # Read predictions to pandas
+
+    out_df = out_df[.9<out_df['confidence']]            # Filter confidence
+
+    if not out_df.empty:                                # Draw rectangles to image
+        for i, row in out_df.iterrows():
+            frame = cv2.rectangle(
+                frame,
+                (int(row['xmin']), int(row['ymin'])),
+                (int(row['xmax']), int(row['ymax'])),
+                (0, 255, 0),
+                1
+            )
+
+        out_df = out_df.reset_index()                   # Set numerical index
+                                                        # Get best case
+        max_thres = out_df.loc[out_df['confidence'].idxmax()]
+
+        center_point = (
+            int(max_thres['xmin'] + (max_thres['xmax'] - max_thres['xmin'])/2),
+            int(max_thres['ymin'] + (max_thres['ymax'] - max_thres['ymin'])/2)
+        )
+
+                                                        # Translate from image to map
+        new_coords, aruco_points, h = get_coords(frame, center_point, new_coord_res=map_size)
+
+        if any(new_coords):                             # If successfull tranlation, draw map
+            map_img = get_empty_img(map_size)
+            map_img = cv2.circle(map_img, new_coords, radius=0, color=(0, 0, 255), thickness=20)
+
+            for i in aruco_points:
+                frame = cv2.putText(frame, str(i), aruco_points[i], cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+
+                                                        # Rezise map to fit frame
+    map_img = cv2.resize(map_img, (frame.shape[0], frame.shape[0])) 
+    numpy_horizontal = np.hstack((
+        add_border(frame),
+        add_border(map_img)
+    ))
+
+    cv2.imshow('preview', numpy_horizontal)             # Show both images
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): break         # Wait for user to type "q" to break
+
+cap.release()
+cv2.destroyAllWindows()
